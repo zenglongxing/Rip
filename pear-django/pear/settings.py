bdd10cdbcbc4bbb1a6c9f6644e6a81bd36bcba45
zenglongxing <975495461@qq.com>
2020-08-18 17:22:38 +0800

    no message

--- pear-django/pear/settings.py
+++ pear-django/pear/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gn0i*!ynw286^il)b#*f985p-lx_6#owio#tvee%e)wi*7!$2u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
